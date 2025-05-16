b1b10bdb0ec40ceb24264fb62d3b48f49fbf9ed1
Apollonia <kitsune3kitsune3@gmail.com>
2025-05-16 09:03:57 +0500

    add test classes and jacoco report

---
+++ src/test/java/praktikum/BunTest.java
@@ -0,0 +1,20 @@
+package praktikum;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class BunTest {
+
+    @Test
+    public void testGetName() {
+        Bun bun = new Bun("black bun", 100);
+        assertEquals("Ошибка: булка должна называться \"black bun\"","black bun", bun.getName());
+    }
+
+    // delta - допустимая погрешность для сравнения чисел с плавающей точкой
+    @Test
+    public void testGetPrice() {
+        Bun bun = new Bun("white bun", 200);
+        assertEquals("Ошибка: цена булки не равна 200",200, bun.getPrice(), 0.001f);
+    }
+}
